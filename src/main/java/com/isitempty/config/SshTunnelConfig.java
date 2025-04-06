3418197d751cdf5ecaefd39e928fdfc417ad7a58
dietken1 <dietken1@ajou.ac.kr>
2025-04-06 19:56:14 +0900

    sshã터널링 코드 수정

--- src/main/java/com/isitempty/config/SshTunnelConfig.java
+++ src/main/java/com/isitempty/config/SshTunnelConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("!serverdb")
+@Profile("serverdb")
 public class SshTunnelConfig {
 
     @Value("${ssh.host:223.130.134.121}")
