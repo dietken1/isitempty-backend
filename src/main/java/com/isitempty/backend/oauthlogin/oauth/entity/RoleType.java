10f7fb4d0770072def76d48472e185894d811dd2
dietken1 <dietken1@ajou.ac.kr>
2025-05-08 01:53:45 +0900

    ROLE TYPE 변경

--- src/main/java/com/isitempty/backend/oauthlogin/oauth/entity/RoleType.java
+++ src/main/java/com/isitempty/backend/oauthlogin/oauth/entity/RoleType.java
@@ -8,8 +8,8 @@ import java.util.Arrays;
 @Getter
 @AllArgsConstructor
 public enum RoleType {
-    USER("ROLE_USER", "일반 사용자 권한"),
-    ADMIN("ROLE_ADMIN", "관리자 권한");
+    USER("USER", "일반 사용자 권한"),
+    ADMIN("ADMIN", "관리자 권한");
 
     private final String code;
     private final String displayName;
