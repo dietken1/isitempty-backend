ea1a742f81693f548e239a3f85ddef0370551328
blueoxygens <exboxer234@gmail.com>
2025-05-10 02:31:56 +0900

    fix : admin token 문제 해결용 코드 수정

--- src/main/java/com/isitempty/backend/oauthlogin/api/controller/user/AdminController.java
+++ src/main/java/com/isitempty/backend/oauthlogin/api/controller/user/AdminController.java
@@ -14,8 +14,6 @@ public class AdminController {
     @PostMapping("/check")
     @PreAuthorize("isAuthenticated()")
     public ResponseEntity<?> check() {
-        // JWT는 서버에서 세션을 관리하지 않으므로 클라이언트에서 토큰 삭제를 처리
-        // 서버에서는 추가적인 블랙리스트 처리(선택 사항)를 구현할 수 있음
         return ResponseEntity.ok("admin 인증 성공");
     }
 }
